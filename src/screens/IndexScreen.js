876e7805df05480c851486967fa2fd04e62b4f5d
Stephen Olayemi <olayemistephen007@gmail.com>
2022-03-03 22:36:55 +0100

    Initial commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>
+                Index Screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen
