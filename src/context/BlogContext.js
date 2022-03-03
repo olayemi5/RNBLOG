592fb6e3c6299452201b266abb1b1f9f3851376b
Stephen Olayemi <olayemistephen007@gmail.com>
2022-03-03 22:49:40 +0100

    Initial commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,13 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+
+    const BlogPost= [
+        {title: "Blog post #1"},
+        {title: "Blog post #2"}
+    ]
+
+    return <BlogContext.Provider value={BlogPost}>
         {children}
     </BlogContext.Provider>
 }
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, { useContext } from 'react'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
+import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
+
+    const blogPosts = useContext(BlogContext);
+
     return (
         <View>
-            <Text>
-                Index Screen
-            </Text>
+            <FlatList 
+                data={blogPosts}
+                keyExtractor={blog => blog.title}
+                renderItem={({ item }) => {
+                    return <Text>
+                        {item.title}
+                    </Text>
+                }}
+            />
         </View>
     )
 }
